e555bf0b82c084d9cb72e4abbba449180d7ca8b0
suhail5746 <suhailbdn2002@gmail.com>
2024-08-29 00:33:02 +0530

    29/8/24

--- Lcs.java
+++ Lcs.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lcs {
     static int lcs_recur(String s1,String s2,int i,int j){
         if(i==0 || j==0)
@@ -16,23 +18,50 @@ public class Lcs {
           return dp[i][j];
         
         if(s1.charAt(i-1)==s2.charAt(j-1))
-          dp[i][j]=lcs_dp(s1,s2,i-1,j-1,dp)+1;
+          return dp[i][j]=lcs_dp(s1,s2,i-1,j-1,dp)+1;
         else
-           dp[i][j]=Math.max(lcs_dp(s1,s2,i,j-1,dp),lcs_dp(s1,s2,i-1,j,dp));
-        return dp[s1.length()][s2.length()];
+           return dp[i][j]=Math.max(lcs_dp(s1,s2,i,j-1,dp),lcs_dp(s1,s2,i-1,j,dp));
+  
         
+    }
+
+    //print lcs
+    static void print_lcs(String s1,String s2,int i,int j,int[][] dp){
+      if(i==0 || j==0)
+          return;
+      if(s1.charAt(i-1)==s2.charAt(j-1)){
+        print_lcs(s1,s2,i-1,j-1,dp);
+        System.out.print(s1.charAt(i-1)+" ");
+        
+      }
+      else if(dp[i][j-1]>dp[i-1][j])
+      {
+        print_lcs(s1,s2,i,j-1,dp);
+      }
+      else
+      {
+        print_lcs(s1,s2,i-1,j,dp);
+      }
+
+  
     }
         
     
         
             
     public static void main(String[] args) {
-        String s1 = "AGGTAB";
-        String s2 = "GXTXAYB";
+        //user input
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first string");
+        String s1=sc.nextLine();
+        System.out.println("Enter second string");
+        String s2=sc.nextLine();
         int m = s1.length();
         int n = s2.length();
         int[][] dp = new int[m+1][n+1];
         System.out.println("Length of lcs_recur is " + lcs_recur(s1, s2, m,n));
         System.out.println("Length of lcs_dp is " + lcs_dp(s1, s2,m,n,dp));
+        print_lcs(s1, s2, m, n, dp);
+
     }
 }
